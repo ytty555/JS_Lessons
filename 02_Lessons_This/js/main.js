e45fedaba223a16e2396fc60f4c881e64bf98fd3
Yuriy Titov <yurtitov@gmail.com>
2019-02-09 20:41:05 +0200

    Замыкания темп-коммит

--- 02_Lessons_This/js/main.js
+++ 02_Lessons_This/js/main.js
@@ -1,9 +1,34 @@
-function makeCounter () {
-    let counter = 0;
-    return function() {
-        return counter++;
-    }
-}
+// function makeCounter () {
+//     let counter = 0;
+//     return function() {
+//         return counter++;
+//     }
+// }
 
-let counter = makeCounter();
-console.log(counter());
+// let counter = makeCounter();
+
+let module = (function() {
+    let counter = 0;
+    
+    function setCounter(value) {
+        counter = value;
+        return counter;
+    }
+
+    function plusCounter() {
+        counter++;
+        return counter;
+    }
+
+    function getCounter() {
+        return counter;
+    }
+
+    return {
+        setCounter: setCounter,
+        getCounter: getCounter,
+        plusCounter: plusCounter
+    }
+})();
+
+console.log(module.getCounter());
