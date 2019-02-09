e0754d72d7f9d7ec0757641085df84f187f66ff3
Yuriy Titov <yurtitov@gmail.com>
2019-02-09 23:02:53 +0200

    Закончил ДЗ по this

--- 02_Lessons_This/js/main.js
+++ 02_Lessons_This/js/main.js
@@ -1,12 +1,3 @@
-// function makeCounter () {
-//     let counter = 0;
-//     return function() {
-//         return counter++;
-//     }
-// }
-
-// let counter = makeCounter();
-
 let module = (function() {
     let counter = 0;
     
@@ -24,11 +15,89 @@ let module = (function() {
         return counter;
     }
 
+    function reset() {
+        counter = 0;
+        return counter;
+    }
+
     return {
         setCounter: setCounter,
         getCounter: getCounter,
-        plusCounter: plusCounter
+        plusCounter: plusCounter,
+        reset: reset
     }
 })();
 
-console.log(module.getCounter());
+// console.log(module.getCounter());
+
+
+// Домашняя работа this
+
+// this #1.
+
+const rectangle = {
+    width: 11,
+    height: 3,
+    getSquare: function() {
+        return this.width * this.height;
+    }
+}
+
+// this #2.
+
+const goods = {
+    price: 10,
+    discount: '15%',
+    getPrice: function () {
+        return price;
+    },
+    getPriceWithDiscount: function() {
+        discountInt = parseInt(this.discount) / 100;
+        return this.price * (1 - discountInt);
+    }
+}
+
+// this #3.
+
+const user = {
+    name: 'Abraham',
+    getName: function() {
+        return getUserName(this);
+    }
+};
+
+function getUserName (Obj) {
+    return Obj.name;
+}
+
+
+// this #4.
+
+let Obj = {
+    height: 10,
+    incHeight: function() {
+        return ++this.height;
+    }
+}
+
+// this #5.
+
+const numerator = {
+    value: 1,
+    double: function () {
+        this.value = this.value * 2;
+        return this;
+    },
+    plusOne: function () {
+        this.value++;
+        return this;
+    },
+    minusOne: function () {
+        this.value--;
+        return this;
+    }
+}
+
+
+let numbers = [4, 12, 0, 10, -2, 4];
+console.log(Math.min.apply(numbers, numbers));
