3c0973501ffd23660841d03048d6bab3db08cf80
Yuriy Titov <yurtitov@gmail.com>
2019-02-13 11:50:41 +0200

    temp

--- 02_Lessons_This/js/main01.js
+++ 02_Lessons_This/js/main01.js
@@ -1,3 +1,4 @@
+'use strict';
 // **********************************************************************
 //    Learn.Javascript.ru - уроки
 // **********************************************************************
@@ -5,83 +6,83 @@
 // Объект калькулятор
 
 const calculator = {
-    read: function () {
-        this.a = +prompt('Input namber A', 0);
-        this.b = +prompt('Input namber B', 0);
-        return this;
-    },
-    sum: function () {
-        return this.a + this.b;
-    },
-    mul: function () {
-        return this.a * this.b;
-    }
+  read: function () {
+    this.a = +prompt('Input namber A', 0);
+    this.b = +prompt('Input namber B', 0);
+    return this;
+  },
+  sum: function () {
+    return this.a + this.b;
+  },
+  mul: function () {
+    return this.a * this.b;
+  }
 };
 
 // Цепочка вызовов
 
 var ladder = {
-    step: 0,
-    up: function() { // вверх по лестнице
-      this.step++;
-      return this;
-    },
-    down: function() { // вниз по лестнице
-      this.step--;
-      return this;
-    },
-    showStep: function() { // вывести текущую ступеньку
-      alert( this.step );
-      return this;
-    }
-  };
+  step: 0,
+  up: function () { // вверх по лестнице
+    this.step++;
+    return this;
+  },
+  down: function () { // вниз по лестнице
+    this.step--;
+    return this;
+  },
+  showStep: function () { // вывести текущую ступеньку
+    alert(this.step);
+    return this;
+  }
+};
 
 
 //   Функция sum()()
 
-function sum (a) {
-    let currentRes = a;
+function sum(a) {
+  let currentRes = a;
 
-    function f (b) {
-        currentRes += b;
-        return f;
-    }
-
-    f.toString = function () {
-        return currentRes;
-    };
-
+  function f(b) {
+    currentRes += b;
     return f;
+  }
+
+  f.toString = function () {
+    return currentRes;
+  };
+
+  return f;
 }
 
 
 // Калькулятор при помощи конструктора
 
-function Calculator () {
+function Calculator() {
   let a = 0;
   let b = 0;
-  
+
   this.read = function () {
       a = +prompt('Input namber A', 0);
       b = +prompt('Input namber B', 0);
       return this;
-  },
-  
-  this.sum = function () {
-      return a + b;
-  },
+    },
 
-  this.mul = function () {
+    this.sum = function () {
+      return a + b;
+    },
+
+    this.mul = function () {
       return a * b;
-  };
+    };
 }
 
-let calc = new Calculator ();
+let calc = new Calculator();
 
 
 // Аккумулятор при помощи конструктора
 
-function Accumulator (startingValue) {
+function Accumulator(startingValue) {
   this.value = startingValue;
 
   this.read = function () {
@@ -89,7 +90,7 @@ function Accumulator (startingValue) {
   };
 }
 
-let acc = new Accumulator (4);
+let acc = new Accumulator(4);
 acc.read();
 alert(acc.value);
 
