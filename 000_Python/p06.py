dfa7220d37e38d90067c175825193e310ff53003
Yuriy Titov <yurtitov@gmail.com>
2019-03-03 00:32:13 +0200

    python

--- 000_Python/p06.py
+++ 000_Python/p06.py
@@ -5,18 +5,23 @@ current_ch = ''
 next_ch = ''
 n = 1
 res_str = ''
+count = ''
 
 for ch in s:
     if ch == current_ch:
         n += 1
         current_ch = ch
-        ch = ''
+        ch_out = ''
     else:
         if n > 1:
-            res_str += str(n)
+            count = str(n)
+            ch_out = ch
+        else:
+            ch_out = ch
         current_ch = ch
         n = 1
-    res_str += ch
+    res_str += count + ch_out
+    count = ''
 
 
 print(res_str)
