6c43ae787cac161a94da095d4f4fb534e1959f50
Yuriy Titov <yurtitov@gmail.com>
2019-03-04 16:57:51 +0200

    Python. Задача решена! )

--- 000_Python/p06.py
+++ 000_Python/p06.py
@@ -1,26 +1,47 @@
-# s = aaaabbcaa
-s = 'aaaabbcaa'
+s = input()
 i = 0
 rep = False
-n = 0
-ch0 = ''
+n = 1
 res_str = ''
+ch0 = ''
+last = False
 
 
 def isLastSymbol(i):
-    return i < len(s)
+    return i < len(s) - 1
+
+def nextEqu(i):
+    if isLastSymbol(i):
+        return s[i] == s[(i + 1)]
+    return False
 
 while i < len(s):
     ch = s[i]
-    if isLastSymbol(i) == False:
+    if isLastSymbol(i):
         if ch == ch0:
             rep = True
             n += 1
         else:
             rep = False
-    if rep:
-        res_str += n
+            n = 1
     else:
-        res_str += ch 
+        if ch == ch0:
+            n += 1
+            res_str += str(n)
+            last = True
+        else:
+            res_str += ch + str(n)
+            last = True
+
+    if rep and last != True:
+        if nextEqu(i) == False:
+            res_str += str(n)
+    elif rep == False and last != True:
+        if nextEqu(i) == False:
+            res_str += ch + str(n)
+        else:
+            res_str += ch
+    ch0 = ch
+    i += 1
 
 print(res_str)
