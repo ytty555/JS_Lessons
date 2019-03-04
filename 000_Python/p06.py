543eea3d3d47b707bad40511a994e08832486688
Yuriy Titov <yurtitov@gmail.com>
2019-03-04 15:37:32 +0200

    temp

--- 000_Python/p06.py
+++ 000_Python/p06.py
@@ -1,27 +1,26 @@
 # s = aaaabbcaa
 s = 'aaaabbcaa'
-
-current_ch = ''
-next_ch = ''
-n = 1
+i = 0
+rep = False
+n = 0
+ch0 = ''
 res_str = ''
-count = ''
 
-for ch in s:
-    if ch == current_ch:
-        n += 1
-        current_ch = ch
-        ch_out = ''
-    else:
-        if n > 1:
-            count = str(n)
-            ch_out = ch
+
+def isLastSymbol(i):
+    return i < len(s)
+
+while i < len(s):
+    ch = s[i]
+    if isLastSymbol(i) == False:
+        if ch == ch0:
+            rep = True
+            n += 1
         else:
-            ch_out = ch
-        current_ch = ch
-        n = 1
-    res_str += count + ch_out
-    count = ''
-
+            rep = False
+    if rep:
+        res_str += n
+    else:
+        res_str += ch 
 
 print(res_str)
