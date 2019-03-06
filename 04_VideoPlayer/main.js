cfbc655a63e287c453f0cb58bfa94fc555aa92e2
Yuriy Titov <yurtitov@gmail.com>
2019-03-07 00:29:43 +0200

    Starting VideoPlayer coding

---
+++ 04_VideoPlayer/main.js
@@ -0,0 +1,24 @@
+class VideoPlayer {
+    constructor() {
+        this.player = document.querySelector('.player');
+        this.video = this.player.querySelector('.viewer');
+        this.progress = document.querySelector('.progress');
+        this.progressBar = this.progress.querySelector('.progress__filled');
+        this.toggle = this.player.querySelector('.toggle');
+        this.skipButtons = this.player.querySelectorAll('[data-skip]');
+        this.ranges = this.player.querySelectorAll('.player__slider');
+    }
+
+    init() {
+        // Start pluging
+    }
+
+    events() {
+        // All events
+        
+    }
+}
+
+const video = new VideoPlayer();
+
+console.log(video)
