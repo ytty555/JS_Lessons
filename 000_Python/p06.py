87658d7642beedec3e48783a1ef38567a3c89c0d
Yuriy Titov <yurtitov@gmail.com>
2019-03-06 07:45:30 +0200

    temp

--- 000_Python/p06.py
+++ 000_Python/p06.py
@@ -1,27 +1,18 @@
 # s = aaaabbcaa
 s = 'aaaabbcaa'
 
-current_ch = ''
+last_ch = ''
 next_ch = ''
 n = 1
 res_str = ''
 count = ''
 
 for ch in s:
-    if ch == current_ch:
+    if ch == last_ch:
         n += 1
-        current_ch = ch
-        ch_out = ''
     else:
-        if n > 1:
-            count = str(n)
-            ch_out = ch
-        else:
-            ch_out = ch
-        current_ch = ch
-        n = 1
-    res_str += count + ch_out
-    count = ''
+        res_str += ch
+        last_ch = ch
 
 
 print(res_str)
