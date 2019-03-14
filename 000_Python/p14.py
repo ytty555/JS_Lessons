49ed4d22f5c20a56d409b3cde3e5ae25d01b74ea
Yuriy Titov <yurtitov@gmail.com>
2019-03-14 11:34:20 +0200

    Сложная задача решена! p14.py

--- 000_Python/p14.py
+++ 000_Python/p14.py
@@ -2,12 +2,9 @@ matrix = []
 
 
 def toInt(arr):
-    if len(arr) == 1:
-        res = int(arr[0])
-    else:
-        res = []
-        for i in range(len(arr)):
-            res.append(int(arr[i]))
+    res = []
+    for i in range(len(arr)):
+        res.append(int(arr[i]))
     return res
 
 
@@ -19,35 +16,25 @@ while True:
         matrix.append(toInt(str))
 
 
-try:
-    if len(matrix) == 1:
-        i = 0
-        while i < len(matrix[0]):
-            A = matrix[0][i - 1]
-            B = matrix[0][i]
-            C = matrix[0][i - len(matrix[0]) + 1]
-            D = matrix[0][i]
-            print(A + B + C + D)
-            i += 1
-    else:
-        i = 0
-        while i < len(matrix):
-            j = 0
-            while j < len(matrix[i]):
-                A = matrix[i][j - 1]
-                B = matrix[i - 1][j]
-                C = matrix[i][j - len(matrix) + 1]
-                D = matrix[i - len(matrix) + 1][j]
-                print(A + B + C + D, end="\t")
-                j += 1
-            i += 1
-            print()
-except:
+if len(matrix) == 1:
     i = 0
-    while i < len(matrix):
-        A = matrix[i - 1]
-        B = matrix[i]
-        C = matrix[i - len(matrix) + 1]
-        D = B
+    while i < len(matrix[0]):
+        A = matrix[0][i - 1]
+        B = matrix[0][i]
+        C = matrix[0][i - len(matrix[0]) + 1]
+        D = matrix[0][i]
         print(A + B + C + D, end="\t")
         i += 1
+else:
+    i = 0
+    while i < len(matrix):
+        j = 0
+        while j < len(matrix[i]):
+            A = matrix[i][j - 1]
+            B = matrix[i - 1][j]
+            C = matrix[i][j - len(matrix[i]) + 1]
+            D = matrix[i - len(matrix) + 1][j]
+            print(A + B + C + D, end="\t")
+            j += 1
+        i += 1
+        print()
