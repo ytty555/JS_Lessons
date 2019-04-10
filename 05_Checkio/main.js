ac96fd363ac99b25e043d60d3365bc42bd47310a
Yuriy Titov <yurtitov@gmail.com>
2019-04-10 23:45:37 +0200

    temp

--- 05_Checkio/main.js
+++ 05_Checkio/main.js
@@ -1,5 +1,35 @@
-function betweenMarkers(text, begin, end) {
-    return text.slice(text.indexOf(begin) + 1, text.indexOf(end));
+function weakPoint(matrix) {
+    let arrRow = [];
+    let arrCol = [];
+    let wRow = 0;
+    let minWRow = 0;
+    let minWCol = 0;
+
+    for (let i = 0; i < matrix.length; i++) {
+        arrCol.push(0);
+    }
+
+    for (let row = 0; row < matrix.length; row++) {
+        wRow = 0;
+        for (let col = 0; col < matrix[row].length; col++) {
+            wRow += matrix[row][col];
+            arrCol[col] += matrix[row][col];
+        }
+        arrRow.push(wRow);
+    }
+
+    let cloneRow = arrRow.slice();
+    let cloneCol = arrCol.slice();
+
+    minWRow = arrRow.sort((a,b) => a - b)[0];
+    minWCol = arrCol.sort((a,b) => a - b)[0];
+
+    return [cloneRow.indexOf(minWRow), cloneCol.indexOf(minWCol)];
 }
 
-console.log(betweenMarkers('What is >apple<', '>', '<'));
+console.log(weakPoint([[7, 2, 7, 2, 8],
+    [2, 9, 4, 1, 7],
+    [3, 8, 6, 2, 4],
+    [2, 5, 2, 9, 1],
+    [6, 6, 5, 4, 5]]
+    ));
